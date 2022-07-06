d7c0d85e33a4a098289f8233e5f76d87e88e8ac7
Pedro Marques <ppmarq1@gmail.com>
2022-07-06 22:18:33 +0100

    Add project

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ const workData = [
       description:
         'This educational project is to build our own web application based on an external API. We selected an API that provides data about a topic that we like and built the web app around it. The web app will have 2 user interfaces.',
       ImgUrl: 'javascriptcapstonegroup.png',
-      tech: ['HTML', 'CSS', 'javascript'],
+      tech: ['HTML', 'CSS', 'javascript', 'Bootstrap', 'Netlify'],
       demoUrl: 'https://ppmarq1.github.io/JavaScript-Group-Capstone/',
       gitUrl: 'https://github.com/ppmarq1/JavaScript-Group-Capstone',
     },
@@ -21,10 +21,10 @@ const workData = [
       name: 'Bookstore React',
       description:
         'This project will lay foundations for Bookstore website. We create the React & Redux part of the app. It was also set up routing using React Router.',
-      ImgUrl: 'work.png',
-      tech: ['Ruby on rails', 'css', 'javascript'],
-      demoUrl: 'https://github.com/EzekielUtshudi/Porfolio-Project',
-      gitUrl: 'https://github.com/EzekielUtshudi/Porfolio-Project',
+      ImgUrl: 'bookstore.png',
+      tech: ['React & Redux', 'HTML', 'CSS', 'javascript'],
+      demoUrl: 'https://github.com/ppmarq1/Bookstore-react',
+      gitUrl: 'https://github.com/ppmarq1/Bookstore-react',
     },
     {
       name: 'Keeping track of hundreds of components 4',
