fa1752b52680d5b26c6a61a264e77db64344b911
Pedro Marques <ppmarq1@gmail.com>
2022-07-07 07:07:53 +0100

    Update files

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const workData = [
   {
-    name: 'Covid-19 Data Hub',
+    name: 'Covid-19 <br> Data Hub',
     description:
         'This is a webapp that display metric data from an API for Covid-19 cases globally. Built with - Javascript, HTML, CSS - React - Redux - API',
     ImgUrl: 'Homecovid.png',
@@ -18,7 +18,7 @@ const workData = [
     gitUrl: 'https://github.com/ppmarq1/JavaScript-Group-Capstone',
   },
   {
-    name: 'Bookstore React Project',
+    name: 'Bookstore <br> React',
     description:
         'This project will lay foundations for Bookstore website. We create the React & Redux part of the app. It was also set up routing using React Router.',
     ImgUrl: 'bookstore.png',
@@ -27,29 +27,29 @@ const workData = [
     gitUrl: 'https://github.com/ppmarq1/Bookstore-react',
   },
   {
-    name: 'Math Magicians Project',
+    name: 'Math <br> Magicians',
     description:
         'A single page app consisting of a calculator. In this project, we created a full website for the Math magicians app, consisting of several pages and using the components in React.',
     ImgUrl: 'math.png',
-    tech: ['HTML', 'CSS', 'JavaScript', 'React'],
+    tech: ['HTML', 'CSS', 'JavaScript', 'React', 'Bootstrap'],
     demoUrl: 'https://62a0bb39f7ea070a0140c8b1--sprightly-faun-8cce82.netlify.app/',
     gitUrl: 'https://github.com/ppmarq1/Math-magicians',
   },
   {
-    name: 'Space Hub Project',
+    name: 'Space Hub  <br> Project Group ',
     description:
         'This is a React app working with the real live data from the SpaceX API. The application will allow users to book rockets and join selected space missions.',
     ImgUrl: 'space.png',
-    tech: ['React & Redux', 'CSS', 'Javascript', 'HTML'],
+    tech: ['React & Redux', 'CSS', 'Javascript', 'HTML', 'Bootstrap', 'Netlify'],
     demoUrl: 'https://62b45e8d1cd8b21af3e0e814--stunning-khapse-f798f8.netlify.app/',
     gitUrl: 'https://github.com/NabilHY/space-hub',
   },
   {
-    name: 'Awesome Books ES6',
+    name: 'Awesome Books <br> ES6',
     description:
         'This is s a simple website built with JavaScript, HTML and CSS, that displays a list of books and allows you to add and remove books from that list. In this Project we have used ES6 modules to write modular JavaScript and understand the improvements that ES6 brings to JavaScript. Also it was used npm as software packages system.',
     ImgUrl: 'awesome.png',
-    tech: ['JavaScript', 'CSS', 'HTML'],
+    tech: ['JavaScript', 'CSS', 'HTML', 'Bootstrap', 'Netlifly'],
     demoUrl: 'https://github.com/ppmarq1/Portfolio-Awesome-Books-ES6',
     gitUrl: 'https://github.com/ppmarq1/Portfolio-Awesome-Books-ES6',
   },
